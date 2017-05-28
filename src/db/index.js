b555c5ef8eeca0d12a2c99d46b9567913219828e
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2017-05-28 17:38:52 +0200

    Make database connections more robust against missing mongodb server

--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,33 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:3101/meteor';
 
+const reconnectIntervalInMs = 1500,
+    socketOptions = {
+        keepAlive: 120
+    },
+    options = {
+        server: {
+            socketOptions,
+        },
+        replset: {
+            socketOptions
+        }
+    };
+
+const db = mongoose.connection;
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URL)
-    .catch(error => {
-        console.error(`Failed to connect to mongodb: ${error}`);
-    })
+
+function handleError(error) {
+    console.error(`MongoDB connection error: ${error}`);
+    setTimeout(connectWithRetry, reconnectIntervalInMs);
+}
+
+function connectWithRetry() {
+    mongoose.connect(MONGO_URL, options)
+        .catch(handleError);
+}
+connectWithRetry();
+
+db.on('error', handleError);
 
 module.exports = mongoose;
