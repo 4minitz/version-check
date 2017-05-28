8fdb452f4b30611dd5440f576871a4cdda3a4c5b
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2017-05-28 08:36:10 +0200

    Add route configuration

---
+++ src/routes.js
@@ -0,0 +1,11 @@
+const versionController = require('./controllers/version');
+
+const routes = {
+    "/updatecheck/:slaveUID/:slaveVersion": {
+        "get"(req, res) {
+            versionController.currentVersion(req, res);
+        }
+    }
+}
+
+module.exports = routes;
