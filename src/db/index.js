bce58e4e1fe7dcc2917fb433691056960f9d015b
Michael Gerhaeuser <michael.gerhaeuser@gmail.com>
2017-05-28 09:32:54 +0200

    Properly handle mongodb connection problems

--- src/db/index.js
+++ src/db/index.js
@@ -1,12 +1,10 @@
 const mongoose = require('mongoose');
 const MONGO_URL = process.env.MONGO_URL || 'mongodb://localhost:3101/meteor';
 
-const db = mongoose.connection;
-db.on('error', (error) => {
-    console.error(`connection error: ${error}`);
-});
-
 mongoose.Promise = global.Promise;
-mongoose.connect(MONGO_URL);
+mongoose.connect(MONGO_URL)
+    .catch(error => {
+        console.error(`Failed to connect to mongodb: ${error}`);
+    })
 
 module.exports = mongoose;
